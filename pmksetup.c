02ce116a0f6dcd4fe6a451872c8ef02b98143546
xsa <xsa@localhost>
2003-03-24 18:53:09 +0000

    find the _must be_ files on the system. Just printing them for now.

--- pmksetup.c
+++ pmksetup.c
@@ -33,22 +33,35 @@
 
 
 #include <sys/param.h>
+#include <sys/types.h>
 #include <sys/utsname.h>
 
 #include <err.h>
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "premake.h"
 
+#define	PATH_DELIMITER	":"	/* string delimiter of the $PATH variable */	
+#define	MAXTOKENS	128	/* max slots in the paths array */
+#define	MAXBINS		4	/* max slots in the binaries array */	
 
-#define PMKSETUP_DEBUG
+
+/* _must be_ binaries to search for */
+static	char *binaries[MAXBINS] = {
+	"ar", "cat", "grep", "install" 
+};
 
 
 /* Local functions declaration */
 int	create_tmp_config(void);
 int	get_env_vars(FILE *);
+int	find_file(char *, char *, char *, int);
+int	strsplit(char *, char **, int);
+
 
 
 int main(int argc, char *argv[]) {
@@ -88,7 +101,6 @@ int create_tmp_config(void) {
 		" and created by pmksetup(8)\n#\n");
 	fprintf(tfd, "# See pmk.conf(5) for more details\n\n");
 
-	/* phase 1: create simple config */
 	get_env_vars(tfd);
 
 	fclose(tfd);
@@ -108,21 +120,90 @@ int create_tmp_config(void) {
 /* Get the environment variables needed for the configuration file */
 
 int get_env_vars(FILE *tfd) { 
-
-	char	*bin_path;
+	int	j;
+	char	*bin_path, *tpath, buf[MAXPATHLEN];
 	struct	utsname	utsname;
 
 	if (uname(&utsname) == -1)
 		err(1, "uname");	
 
-	if ((bin_path = getenv("PATH")) == NULL) 
+	if ((bin_path = getenv("PATH")) == NULL)
 		err(1, "getenv");
 
 	/* Temporarily printing the variables */
 	fprintf(tfd, "OS_NAME=%s\n", utsname.sysname);
 	fprintf(tfd, "OS_RELEASE=%s\n", utsname.release);
 	fprintf(tfd, "ARCH=%s\n", utsname.machine);
-	fprintf(tfd, "BIN_PATH=%s\n", bin_path);		
+	fprintf(tfd, "BIN_PATH=%s\n", bin_path);
 
-        return (0);
+	/* 
+	 * XXX should store the above variables in a hash instead of
+	 * looping here. 
+	 */ 
+	for (j = 0; j < MAXBINS; j++) {
+		tpath = strdup(bin_path);
+		if (find_file(tpath, binaries[j], buf, MAXPATHLEN) == 0)
+			fprintf(stderr, "%s => %s\n", binaries[j], buf);
+		else
+			fprintf(stderr, "%s not found\n",binaries[j]);
+	}
+	return(0);
+}
+
+/* 
+ * find a file in the binary path; 
+ * atm it just prints the path to the binary we are looking for.
+ *
+ *	bin_path : path style string
+ *	file_name : name of the file to search
+ *	file_path : storage of the full path if find
+ *	fp_len : size of the storage
+ */
+
+int find_file(char *bin_path, char *file_name, char *file_path, int fp_len) {
+ 	DIR	*dirp;
+	struct	dirent	*dp;
+
+	int	i, s;
+	char	*path[MAXTOKENS];  
+
+	s = strsplit(bin_path, path, MAXTOKENS);
+
+	for (i = 0; i < s; i++) {
+		if (!(dirp = opendir(path[i])))
+			err(1, "opendir");
+	
+		while ((dp = readdir(dirp)) != NULL) {
+			if (dp->d_ino == 0)
+				continue;
+						
+			if ((strncmp(dp->d_name, file_name, fp_len) == 0) &&  
+				(snprintf(file_path, fp_len, "%s/%s", 
+					path[i], file_name) < fp_len)) {
+						closedir(dirp);
+						return(0);
+			}
+		}
+		closedir(dirp);
+	}
+	return(-1);
+}
+
+
+/* 
+ * split the $PATH environment variable using PATH_DELIMITER as 
+ * the string delimiter.
+ * Returns the number of elements of the array.
+ */
+
+int strsplit(char *path, char **tokens, int tsize) {
+	int	i = 0;
+	char	*p, *last;
+
+	for ((p = strtok_r(path, PATH_DELIMITER, &last)); p;
+		(p = strtok_r(NULL, PATH_DELIMITER, &last)), i++) {
+			if (i < tsize -1)
+				tokens[i] = p;
+	}
+	return(i);
 }
