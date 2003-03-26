df8adf60aeedc539eefde053f5be5dee2387a29a
mipsator <mipsator@localhost>
2003-03-26 18:35:09 +0000

    - create compat/ directory - add compat.h for compatibility with functions that are not ansi/posix - add strlcpy.c (import from OpenBSD project)

---
+++ compat/compat.h
@@ -0,0 +1,56 @@
+/* $Id$ */
+
+/*
+ * Copyright (c) 2003 Damien Couderc
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#ifndef _PMK_COMPAT_H_
+#define _PMK_COMPAT_H_
+
+/*
+	strlcpy
+
+	Systems known to have compatible strlcpy in string.h :
+		OpenBSD		since 2.4
+		FreeBSD		since 3.3
+		NetBSD		since 1.5
+		SCO OpenServer	since 5.0.6
+		SunOs		since 5.8
+
+	Note: Feel free to send us a notice if your system is not
+		listed here but should be.
+
+
+	If your system has no compatible strlcpy uncomment the following include.
+*/
+
+/* #include "strlcpy.c" */
+
+#endif /* _PMK_COMPAT_H_ */
