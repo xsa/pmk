daf97f9dfc4d756de0bd3d160a3f13eed43aca85
mipsator <mipsator@localhost>
2003-08-11 18:31:58 +0000

    add pmk objects.

---
+++ pmk_obj.h
@@ -0,0 +1,62 @@
+/* $Id$ */
+
+/*
+ * Copyright (c) 2003 Damien Couderc
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#ifndef _PMK_OBJECT_H_
+#define _PMK_OBJECT_H_
+
+#include "dynarray.h"
+#include "hash.h"
+
+typedef unsigned char	potype;
+
+typedef struct {
+	potype	 type;
+	void	*data;
+} pmkobj;
+
+/* define pmk object types */
+#define PO_NULL		0
+#define PO_STRING	1
+#define PO_LIST		2
+#define PO_HASH		3
+
+
+pmkobj	*mk_obj_str(char *);
+pmkobj	*mk_obj_list(dynary *);
+pmkobj	*mk_obj_hash(htable *);
+/*pmkobj	*mk_obj_bool(bool);*/
+potype	get_obj_type(pmkobj *);
+void	*get_obj_data(pmkobj *);
+void	 obj_free(pmkobj *);
+
+#endif /* _PMK_OBJECT_H_ */
