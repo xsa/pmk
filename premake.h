fba62249b4fe8257179654c0f1e7d4f3db013e78
mipsator <mipsator@localhost>
2003-11-18 16:00:11 +0000

    bump 0.7.3 snapshot.

--- premake.h
+++ premake.h
@@ -84,7 +84,7 @@
 
 #define PREMAKE_MAJOR		"0"
 #define PREMAKE_MINOR		"7"
-#define PREMAKE_SNAP		"2" /* only used for snapshots */
+#define PREMAKE_SNAP		"3" /* only used for snapshots */
 #define PREMAKE_VERSION		PREMAKE_MAJOR "." PREMAKE_MINOR
 
 #define PREMAKE_FILENAME	"pmkfile"
