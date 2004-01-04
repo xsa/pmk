c48964557385cdec797b381ea7ad65648b63e689
mipsator <mipsator@localhost>
2004-01-04 21:05:08 +0000

    cosmetics.

--- func.c
+++ func.c
@@ -976,8 +976,9 @@ bool pmk_check_pkg_config(pmkcmd *cmd, htable *ht, pmkdata *pgd) {
 	/* try to get pkg-config lib path from pmk.conf */
 	pc_path = hash_get(pgd->htab, PMKCONF_PC_PATH_LIB);
 	if (pc_path == NULL) {
-		pmk_log("Unable to find pkg-config libdir.\n");
-		pmk_log("WARNING : pkg-config may not be installed or need to update pmk.conf.\n");
+		pmk_log("\tUnable to find pkg-config libdir.\n");
+		pmk_log("\tWARNING : pkg-config may not be installed\n");
+		pmk_log("\t or pmk.conf need to be updated.\n");
 		return(false);
 	}
 #endif
