fea378acb3bd1df7bdaa3a313584503a48b04e47
Xavier Santolaria <xavier@santolaria.net>
2004-01-09 14:27:06 +0000

    add my (c) for 2004; ok Damien;

--- pmkscan.c
+++ pmkscan.c
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (c) 2003 Damien Couderc
+ * Copyright (c) 2004 Xavier Santolaria
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
