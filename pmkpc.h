38cab880a4057b6a619637a7e699e26072fc83a8
mipsator <mipsator@localhost>
2004-01-12 20:00:09 +0000

    imported preliminary code for our pkg-config alternative.

---
+++ pmkpc.h
@@ -0,0 +1,65 @@
+/* $Id$ */
+
+/*
+ * Copyright (c) 2004 Damien Couderc
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *    - Neither the name of the copyright holder(s) nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#ifndef _PMKPC_H_
+#define _PMKPC_H_
+
+
+/* options id */
+#define PMKPC_OPT_UNKNOWN	0
+#define PMKPC_OPT_VERSION	1
+#define PMKPC_OPT_MODVERS	2
+#define PMKPC_OPT_CFLAGS	3
+#define PMKPC_OPT_LIBS		4
+#define PMKPC_OPT_EXISTS	5
+
+#define PMKPC_COMPAT_VERSION	"0.15.0"
+
+
+typedef struct {
+	char		*name;
+	bool		 arg;
+	unsigned int	 id;
+} pcopt;
+
+typedef struct {
+	unsigned int	 idx,
+			 id;
+	char		*arg;
+} optcell;
+
+#endif /* _PMKPKGCFG_H_ */
+
