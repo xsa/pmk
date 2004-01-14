326386b726a8d3509c69f73ecfadb52bcb915900
mipsator <mipsator@localhost>
2004-01-14 15:36:33 +0000

    improved pmkpc to fall back on config tool if module is not found.

--- pmkpc.c
+++ pmkpc.c
@@ -38,8 +38,10 @@
 #include <stdlib.h>
 
 #include "compat/pmk_string.h"
+#include "cfgtool.h"
 #include "common.h"
 #include "dynarray.h"
+#include "parse.h"
 #include "pkgconfig.h"
 #include "pmkpc.h"
 #include "premake.h"
@@ -153,20 +155,45 @@ debugf("{pcgetopt} idx = %d", idx);
 }
 
 /*
+	clean
+*/
+
+void clean(pcdata *p_cd) {
+	/* clean data */
+	if (p_cd->pda != NULL) {
+		da_destroy(p_cd->pda);
+	}
+	if (p_cd->pht != NULL) {
+		hash_destroy(p_cd->pht);
+	}
+	if (p_cd->ppd != NULL) {
+		pkgdata_destroy(p_cd->ppd);
+	}
+}
+
+/*
 	XXX
 */
 
 int main(int argc, char *argv[]) {
+	FILE		*fp;
 	bool		 opt_version = false,
 			 opt_modvers = false,
 			 opt_exists = false,
 			 opt_cflags = false,
 			 opt_libs = false;
-	char		*mod;
-	dynary		*pda;
+	cfgtcell	*pcc = NULL;
+	cfgtdata	*pcd = NULL;
+	char		*mod,
+			*bpath,
+			*opt,
+			*pc_path,
+			 pc_cmd[MAXPATHLEN],
+			 pc_buf[MAXPATHLEN],
+			 buf[TMP_BUF_LEN];
 	optcell		*poc;
+	pcdata		 gdata;
 	pkgcell		*ppc;
-	pkgdata		*ppd;
 	unsigned int	 i;
 
 	poc = optcell_init();
@@ -175,17 +202,17 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	pda = da_init();
-	if (pda == NULL) {
+	gdata.pda = da_init();
+	if (gdata.pda == NULL) {
 		optcell_destroy(poc);
 		errorf("cannot init dynary.");
 		exit(EXIT_FAILURE);
 	}
 
-	ppd = pkgdata_init();
-	if (ppd == NULL) {
+	gdata.ppd = pkgdata_init();
+	if (gdata.ppd == NULL) {
 		optcell_destroy(poc);
-		da_destroy(pda);
+		clean(&gdata);
 		errorf("cannot init pkgdata.");
 		exit(EXIT_FAILURE);
 	}
@@ -220,6 +247,7 @@ debugf("{main} id = %d", poc->id);
 				case PMKPC_OPT_UNKNOWN :
 				default :
 					optcell_destroy(poc);
+					clean(&gdata);
 					errorf("unknown option '%s'.", poc->arg);
 					exit(EXIT_FAILURE);
 					break;
@@ -229,7 +257,7 @@ debugf("{main} id = %d", poc->id);
 debugf("{main} new mod = '%s'", argv[poc->idx]);
 #endif
 			/* add new module */
-			da_push(pda, strdup(argv[poc->idx])); /* XXX check */
+			da_push(gdata.pda, strdup(argv[poc->idx])); /* XXX check */
 			poc->idx++;
 		}
 	}
@@ -238,25 +266,52 @@ debugf("{main} new mod = '%s'", argv[poc->idx]);
 
 	if (opt_version == true) {
 		printf("%s\n", PMKPC_COMPAT_VERSION);
+		clean(&gdata);
 		exit(EXIT_SUCCESS);
 	}
 
-	/*scan_dir(PKGCONFIG_DIR, ppd);*/
-	pkg_collect("/usr/local/lib/pkgconfig", ppd); /* nice hardcode isn't it ? :) */
+	/* initialise global data hash table */
+	gdata.pht = hash_init(MAX_DATA_KEY);
+	if (gdata.pht == NULL) {
+		clean(&gdata);
+		errorf("cannot initialize hash table for data.");
+		exit(EXIT_FAILURE);
+	}
+
+	fp = fopen(PREMAKE_CONFIG_PATH, "r");
+	if (fp != NULL) {
+		if (parse_pmkconf(fp, gdata.pht, PRS_PMKCONF_SEP, process_opt) == false) {
+			/* parsing failed */
+			clean(&gdata);
+			errorf("failed to parse '%s'.", PREMAKE_CONFIG_PATH);
+			exit(EXIT_FAILURE);
+		}
+		fclose(fp);
+	}
+
+	/* try to get pkg-config lib path from pmk.conf */
+	pc_path = hash_get(gdata.pht, PMKCONF_PC_PATH_LIB);
+	if (pc_path == NULL) {
+		printf("Unable to find pkg-config libdir.\n");
+		clean(&gdata);
+		exit(EXIT_FAILURE);
+	}
+
+	pkg_collect(pc_path, gdata.ppd); /* nice hardcode isn't it ? :) */
 
 #ifdef DEBUG_PMKPC
-debugf("{main} usize = '%d'", da_usize(pda));
+debugf("{main} usize = '%d'", da_usize(gdata.pda));
 #endif
-	for (i = 0 ; i < da_usize(pda) ; i++) {
-		mod = da_idx(pda, i);
+	for (i = 0 ; i < da_usize(gdata.pda) ; i++) {
+		mod = da_idx(gdata.pda, i);
 #ifdef DEBUG_PMKPC
 debugf("{main} mod = '%s' (i = %d)", mod, i);
 #endif
-		if (pkg_mod_exists(ppd, mod) == true) {
+		if (pkg_mod_exists(gdata.ppd, mod) == true) {
 #ifdef DEBUG_PMKPC
 debugf("module '%s' found", mod);
 #endif
-			ppc = pkg_cell_add(ppd, mod);
+			ppc = pkg_cell_add(gdata.ppd, mod); /* ppc is part of ppd, don't destroy */
 
 			if (opt_modvers == true) {
 				/* print module version */
@@ -265,35 +320,122 @@ debugf("module '%s' found", mod);
 
 			if ((opt_cflags == true) || (opt_libs == true)) {
 				/* get cflags and/or libs */
-				if (pkg_recurse(ppd, mod) == false) {
+				if (pkg_recurse(gdata.ppd, mod) == false) {
+					clean(&gdata);
 					errorf("failed on recurse !");
-					da_destroy(pda);
-					pkgdata_destroy(ppd);
+					exit(EXIT_FAILURE);
+				}
+
+				if (opt_cflags == true) {
+					printf("%s", pkg_get_cflags(gdata.ppd));
+				}
+				if (opt_libs == true) {
+					printf("%s", pkg_get_libs(gdata.ppd));
+				}
+				printf("\n");
+			}
+		} else {
+#ifdef DEBUG_PMKPC
+debugf("module not found");
+#endif
+
+			/* init config tool data if needed */
+			if (pcd == NULL) {
+				pcd = parse_cfgt_file();
+				if (pcd == NULL) {
+					clean(&gdata);
+					errorf("unable to load config tool data.");
 					exit(EXIT_FAILURE);
 				}
 			}
 
+			/* set config tool filename */
+			if (cfgtcell_get_binary(pcd, mod, pc_cmd, sizeof(pc_cmd)) == false) {
+				snprintf(pc_cmd, sizeof(pc_cmd), "%s-config", mod); /* XXX check */
+			}
 
-		} else {
-			printf("module not found\n");
+			bpath = hash_get(gdata.pht, PMKCONF_PATH_BIN);
+			if (bpath == NULL) {
+				/* XXX */
+				exit(EXIT_FAILURE);
+			}
+
+			pcc = cfgtcell_get_cell(pcd, pc_cmd);
+
+			/* looking for it in the path */
+			if (get_file_path(pc_cmd, bpath, pc_buf, sizeof(pc_buf)) == true) {
+				/* use CHECK_CONFIG */
+#ifdef DEBUG_PMKPC
+debugf("Found alternative '%s' tool.", pc_cmd);
+#endif
+
+				if (opt_modvers == true) {
+					/* check if specific option exists */
+					if ((pcc != NULL) && (pcc->version != NULL)) {
+						opt = pcc->version;
+					} else {
+						opt = CFGTOOL_OPT_VERSION;
+					}
+
+					if (ct_get_version(pc_buf, opt, buf, sizeof(buf)) == false) {
+						clean(&gdata);
+						errorf("cannot get version from '%s'.", pc_cmd);
+						exit(EXIT_FAILURE);
+					} else {
+						printf("%s\n", buf);
+					}
+				}
+
+				/* cflags and libs stuff */
+				if ((opt_cflags == true) || (opt_libs == true)) {
+					if (opt_cflags == true) {
+						/* check if there is a special option */
+						if ((pcc != NULL) && (pcc->cflags != NULL)) {
+							opt = pcc->cflags;
+						} else {
+							opt = CFGTOOL_OPT_CFLAGS;
+						}
+
+						/* get cflags from config tool */
+						if (ct_get_data(pc_buf, opt, "", buf, sizeof(buf)) == false) {
+							errorf("cannot get CFLAGS.");
+							exit(EXIT_FAILURE);
+						} else {
+							printf("%s ", buf);
+						}
+					}
+					if (opt_libs == true) {
+						/* check if there is a special option */
+						if ((pcc != NULL) && (pcc->libs != NULL)) {
+							opt = pcc->libs;
+						} else {
+							opt = CFGTOOL_OPT_LIBS;
+						}
+
+						/* get libs from config tool */
+						if (ct_get_data(pc_buf, opt, "", buf, sizeof(buf)) == false) {
+							errorf("cannot get LIBS.");
+							exit(EXIT_FAILURE);
+						} else {
+							printf("%s ", buf);
+						}
+					}
+					printf("\n");
+				}
+			} else {
+				errorf("'%s' not found.", pc_cmd);
+				exit(EXIT_FAILURE);
+			}
 		}
 	}
-
-	if ((opt_cflags == true) || (opt_libs == true)) {
-		if (opt_cflags == true) {
-			printf("%s", pkg_get_cflags(ppd));
-		}
-		if (opt_libs == true) {
-			printf("%s", pkg_get_libs(ppd));
-		}
-		printf("\n");
-	}
-
 #ifdef DEBUG_PMKPC
 debugf("{main} destroy data");
 #endif
-	da_destroy(pda);
-	pkgdata_destroy(ppd);
+	clean(&gdata);
+	if (pcd != NULL) {
+		/* XXX msg */
+		cfgtdata_destroy(pcd);
+	}
 
 #ifdef DEBUG_PMKPC
 debugf("End.");
