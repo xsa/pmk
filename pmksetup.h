71eab8fe826a9b323fbed450143a4bc9ea35c1ec
mipsator <mipsator@localhost>
2004-02-12 14:11:33 +0000

    update my copyrights.

--- pmksetup.h
+++ pmksetup.h
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (c) 2003-2004 Xavier Santolaria 
+ * Copyright (c) 2003-2004 Damien Couderc
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
