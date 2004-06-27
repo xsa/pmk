7191744a058c21ddcca334f7834371a82d13ee04
mipsator <mipsator@localhost>
2004-06-27 10:54:31 +0000

    fix date.

--- hash_tools.h
+++ hash_tools.h
@@ -1,7 +1,7 @@
 /* $Id$ */
 
 /*
- * Copyright (c) 2003-2004 Damien Couderc
+ * Copyright (c) 2004 Damien Couderc
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
