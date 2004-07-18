d4cac826885a79ced2a76356b5043cd176845229
mipsator <mipsator@localhost>
2004-07-18 12:29:40 +0000

    added option to parse config file in parser_test.

--- tests/parser_test.c
+++ tests/parser_test.c
@@ -21,25 +21,105 @@
 #define DEBUG_PRS	1
 #include "../parse.c"
 
+
+/*
+	process option line of configuration file
+
+	pht : storage hash table
+	popt : option structure to record
+
+	return : boolean
+*/
+
+bool process_fake(htable *pht, prsopt *popt) {
+	/*if ((popt->opchar != CHAR_COMMENT) && (popt->opchar != CHAR_EOS)) {                     */
+	/*        |+ add options that are not comment neither blank lines +|                      */
+	/*        if (hash_update_dup(pht, popt->key, po_get_str(popt->value)) == HASH_ADD_FAIL) {*/
+	/*                errorf("hash failure.");                                                */
+	/*                return(false);                                                          */
+	/*        }                                                                               */
+	/*}                                                                                       */
+	return(true);
+}
+
+/*
+	usage
+*/
+
+void usage(void) {
+	fprintf(stderr, "usage: parser_test [-ch] [file path]\n");
+}
+
+/*
+	main loop
+*/
+
 int main(int argc, char *argv[]) {
 	FILE	*fd;
-	bool	 rval;
+	bool	 loop = true,
+		 parse_cfg = false,
+		 rval;
+	char	 chr;
 	prsdata	*pdata;
+	htable	*pht;
 
-	if (argc != 2) {
+	while (loop == true) {
+		chr = getopt(argc, argv, "ch");
+		if (chr == -1) {
+			loop = false;
+		} else {
+			switch (chr) {
+				case 'c' :
+					/* parse a config file */
+					parse_cfg = true;
+					break;
+
+				case 'h' :
+				case '?' :
+				default :
+					usage();
+					exit(EXIT_FAILURE);
+					/* NOTREACHED */
+			}
+		}
+	}
+
+	argc = argc - optind;
+	argv = argv + optind;
+
+	if (argc != 1) {
 		printf("filename not provided.\n");
 		exit(EXIT_FAILURE);
 	}
 
 	pdata = prsdata_init();
 
-	fd = fopen(argv[1], "r");
+	fd = fopen(argv[0], "r");
 	if (fd == NULL) {
-		errorf("cannot open '%s'.", argv[1]);
+		errorf("cannot open '%s'.", argv[0]);
 		return(false);
 	}
 
-	rval = parse_pmkfile(fd, pdata, kw_pmkfile, nbkwpf);
+	if (parse_cfg == false) {
+		rval = parse_pmkfile(fd, pdata, kw_pmkfile, nbkwpf);
+
+		printf("cleaning parsing tree ... ");
+		prsdata_destroy(pdata);
+
+		printf("ok\n");
+	} else {
+		pht = hash_init_adv(1024, NULL, (void (*)(void *)) prsopt_destroy, NULL);
+		if (pht == NULL) {
+			printf("cannot create hash table.\n");
+			exit(EXIT_FAILURE);
+		}
+
+		rval = parse_pmkconf(fd, pht, PRS_PMKCONF_SEP, process_fake);
+
+		printf("cleaning hash table ... ");
+		hash_destroy(pht);
+		printf("ok\n");
+	}
 	fclose(fd);
 
 	if (rval == true) {
@@ -47,10 +127,6 @@ int main(int argc, char *argv[]) {
 	} else {
 		printf("Parsing failed.\n");
 	}
-
-	printf("cleaning parsing tree ... ");
-	prsdata_destroy(pdata);
-	printf("ok\n");
 
 	return(0);
 }
