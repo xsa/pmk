c22f0f2c1b00743663469150c7640d644c9c08b3
mipsator <mipsator@localhost>
2005-06-13 18:55:49 +0000

    update makefile and add new tests in path_test.c.

--- tests/path_test.c
+++ tests/path_test.c
@@ -7,17 +7,19 @@
 #include <sys/param.h>
 #include <stdio.h>
 
-#include "../compat/compat.h"
+#include "../compat.h"
 #include "../compat/pmk_stdbool.h"
 #include "../pathtools.h"
 
-char	*chk_paths[4] = {
+char	*chk_paths[] = {
 		"/usr/./local/bin/../lib//",
 		"/usr/../../local",
 		"/usr////lib/..",
+		"./youpi/youpla/../paf",
+		"blah/../odidonk",
 		"/var/."
 	},
-	*rel_paths[4][2] = {
+	*rel_paths[][2] = {
 		{"/home/me/tmp/src/youpi", "/home/me/tmp/build/"},
 		{"/usr/local/lib/", "/var/named"},
 		{"/","/etc/ppp"},
@@ -25,20 +27,23 @@ char	*chk_paths[4] = {
 	};
 
 
-
-
 int main(void) {
-	char	buf[MAXPATHLEN];
-	int	i;
+	char			buf[MAXPATHLEN];
+	unsigned int	i,
+					s;
+
+	s = (unsigned int) (sizeof(chk_paths) / sizeof(char *));
 
 	printf("* Checking paths :\n");
-	for (i = 0 ; i < 4 ; i++) {
+	for (i = 0 ; i < s ; i++) {
 		chkpath(chk_paths[i], buf); /* XXX check ? */
 		printf("\t'%s' => '%s'\n", chk_paths[i], buf);
 	}
 
+	s = (unsigned int) (sizeof(rel_paths) / (sizeof(char *) * 2));
+
 	printf("* Test relative paths :\n");
-	for (i = 0 ; i < 4 ; i++) {
+	for (i = 0 ; i < s ; i++) {
 		printf("\tfrom = '%s', to = '%s'\n", rel_paths[i][0], rel_paths[i][1]);
 		relpath(rel_paths[i][0], rel_paths[i][1], buf);
 		printf("\trelpath = '%s'\n\n", buf);
@@ -46,3 +51,4 @@ int main(void) {
 
 	return(0);
 }
+
