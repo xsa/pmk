aa9eec70a5c11991a96e37f03d747b8b0bc837e2
mipsator <mipsator@localhost>
2005-08-20 12:38:34 +0000

    fix warning.

--- compat.c
+++ compat.c
@@ -1428,7 +1428,7 @@ printf("Enter state PARSE_CONV_SPEC\n");
 						SET_FLAG(FLAG_ALTERNATIVE_FORM);
 */
 
-						convert_int(&data, (unsigned_t) ptr);
+						convert_int(&data, (unsigned_t) (unsigned int) ptr);
 						break;
 
 					case 'n' :
