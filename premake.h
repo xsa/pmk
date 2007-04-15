d50025c603038f353d047a612c98509c8ae1ca82
coudercd <coudercd@localhost>
2007-04-15 15:17:24 +0000

    bump pmk 0.10.2 release

--- premake.h
+++ premake.h
@@ -72,7 +72,7 @@
 /* minor version */
 #define PREMAKE_VMINOR	"10"
 /* minor subversion, only used for bugfixes  */
-#define PREMAKE_VSUB	"1"
+#define PREMAKE_VSUB	"2"
 
 /* full version */
 #ifndef PREMAKE_VSUB
