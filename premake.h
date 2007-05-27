92ab29c2307ea7fcea60f0628eb7ca6a7ad0fd51
coudercd <coudercd@localhost>
2007-05-27 10:54:30 +0000

    fix use of LIBS

--- premake.h
+++ premake.h
@@ -72,7 +72,7 @@
 /* minor version */
 #define PREMAKE_VMINOR	"10"
 /* minor subversion, only used for bugfixes  */
-#define PREMAKE_VSUB	"2"
+#define PREMAKE_VSUB	"pre3"
 
 /* full version */
 #ifndef PREMAKE_VSUB
