42e6cbab4bd56465996849c4ba941cc6e2e3b3d3
coudercd <coudercd@localhost>
2007-12-03 20:11:07 +0000

    bump to version 0.10.3

--- premake.h
+++ premake.h
@@ -72,7 +72,7 @@
 /* minor version */
 #define PREMAKE_VMINOR	"10"
 /* minor subversion, only used for bugfixes  */
-#define PREMAKE_VSUB	"pre3"
+#define PREMAKE_VSUB	"3"
 
 /* full version */
 #ifndef PREMAKE_VSUB
